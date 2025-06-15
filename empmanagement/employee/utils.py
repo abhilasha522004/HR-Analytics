62fd6ed13e157c4224783b6c4d8a5255bb72a662
abhilasha522004 <abhilasha522004@gmail.com>
2025-06-15 20:10:55 +0530

    new commits

--- empmanagement/employee/utils.py
+++ empmanagement/employee/utils.py
@@ -6,7 +6,7 @@ def send_email_to_employees(subject, message, recipient_list):
     send_mail(
         subject,
         message,
-        getattr(settings, 'EMAIL_HOST_USER', None),  # From email (None for console backend)
+        getattr(settings, 'EMAIL_HOST_USER', None), 
         recipient_list,
         fail_silently=False,
     )
